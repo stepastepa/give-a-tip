3b6dfa66568d161deabcd567405d17fc17ed943e
stepastepa <stepanisworking@gmail.com>
2026-02-11 02:55:48 +0200

    fix: improve pseudo random function

--- docs/seeded-random.js
+++ docs/seeded-random.js
@@ -1,11 +1,9 @@
 /* ===== Seeded random ===== */
 function mulberry32(seed) {
-  return function () {
-    let t = seed += 0x6D2B79F5;
-    t = Math.imul(t ^ t >>> 15, t | 1);
-    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
-  };
+  let t = seed += 0x6D2B79F5;
+  t = Math.imul(t ^ t >>> 15, t | 1);
+  t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+  return ((t ^ t >>> 14) >>> 0) / 4294967296;
 }
 
 /* ===== String to number ===== */
@@ -28,8 +26,7 @@ function newSeed(xxx) {
 
 function seededRandom(yyy) {
   let seed = newSeed(yyy); 
-  let x = mulberry32(seed);
-  return x();
+  return mulberry32(seed);
 }
 
 export { seededRandom };
